1df3ec7be8debabf17614c0ee3e6b5423a6cf3f0
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-06-16 14:32:26 +0700

    Description for example data

---
+++ R/data_example.R
@@ -0,0 +1,10 @@
+#' HRV report variables description
+#'
+#' Description of variables for output of
+#'
+#'
+#'
+#' @format A data.frame with the following columns:
+#' * \strong{variable}: variable name
+#' * \strong{description}: description of the variable
+"HRV_report_vars"
