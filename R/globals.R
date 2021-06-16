9ca7df181e08118e274bcbad59927ac08b343e54
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-06-16 14:31:29 +0700

    Add Global vars

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+### Global variables
+
+utils::globalVariables(c("text", "Channel", "Gender", "Age", "Beats_tot", "Rec_length",
+                         "Discontinuities", "Power_tot", "VLF", "LF","LF_nu", "HF",
+                         "HF_nu", "LF_HF"))
